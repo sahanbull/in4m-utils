3caad26d14d200f2cdd0e661503b81c8dcf256ec
in4maniac <sahanbull@gmail.com>
2017-03-25 13:19:48 +0000

    setup: added the setup file with version 0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='in4m-utils',
+    version='0.0.1',
+    packages=['test', 'in4m_utils', 'in4m_utils.aws', 'in4m_utils.general', 'test-integration'],
+    url='in4maniac.wordpress.com',
+    license='MIT',
+    author='in4maniac',
+    author_email='sahanbull@gmail.com',
+    description='A general purpose utilities libraries to do common python operations'
+)
